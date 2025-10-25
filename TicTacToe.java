56a856645356830aaf7a645589bc49f8739a0c05
kamillelek <kamilxlelek@gmail.com>
2025-10-26 01:01:49 +0200

    updated

--- TicTacToe.java
+++ TicTacToe.java
@@ -27,9 +27,10 @@ public class TicTacToe {
 
         System.out.println("=== TIC TAC TOE ===");
         printBoard(board);
+        move(currentPlayer,scanner, gameEnded);
 
         while (!gameEnded) {
-
+            hasWon(currentPlayer);
         }
 
         scanner.close();
@@ -50,28 +51,60 @@ public class TicTacToe {
     }
 
     // Check if a player has won
-    public static boolean hasWon(char player) {
-        //jezeli ten sam znak gracza jest 3 pola z zrzedu obok siebie
-        //musimy odczytywac pola
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++){
-
+    public static boolean hasWon(char currentPlayer) {
+        int count = 0;
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                if (board[i][j] == currentPlayer){
+                    count++;
+                    if (count == 3){
+                        System.out.println("The winner is " + currentPlayer);
+                    }
+                }
+            }
         }
-    }
-
-
-
-
-
-
-
-
         return true;
     }
 
     // Check if the board is full
-    public static boolean isDraw() {
-       return true;
+    public static boolean isDraw(char currentPlayer) {
+        int count = 0;
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                if (board[i][j] == currentPlayer){
+                    count++;
+                    if (count == 9){
+                        System.out.println("It's a draw! ");
+                    }
+                }
+            }
+        }
+        return true;
     }
+
+    //move
+    public static boolean move(char currentPlayer, Scanner scanner, boolean gameEnded) {
+        while (true) {
+            char lastMove = currentPlayer;
+            System.out.println(currentPlayer + " Your move!");
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+            if (row>3 || col>3 && row<0 || col<0){
+                System.out.println("Invalid move!");
+            }
+            board[row][col] = currentPlayer;
+            System.out.println("Your move is " + board[row][col]);
+            printBoard(board);
+            hasWon(currentPlayer);
+            isDraw(currentPlayer);
+            if(lastMove == 'X'){
+                currentPlayer = 'O';
+            }
+            if (lastMove == 'O'){
+                currentPlayer = 'X';
+            }
+        }
+    }
+
 }
 
